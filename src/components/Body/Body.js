9d60e30ba3bd978ee19dc4ff33db83d2789f0254
PhilGebauer <gebauer.phil@gmail.com>
2018-10-06 13:26:17 -0500

    130 commit

--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -11,13 +11,25 @@ class Body extends Component {
 
     addUsername = () =>  {
         const usernameIn = document.getElementById( 'usernameIn' ).value;
-        // this.state.test = usernameIn; -> this wont work
-        // this.setState( { test: usernameIn});
-        let usernames = this.state.usernames;
-        usernames.push( usernameIn );
-        this.setState( { usernames } );
+        if (usernameIn === '') {
+            alert('no empties')
+        } else  {
+            // this.state.test = usernameIn; -> this wont work
+            // this.setState( { test: usernameIn});
+            let usernames = this.state.usernames;
+            usernames.push( usernameIn );
+            this.setState( { usernames } );
+            document.getElementById( 'usernameIn' ).value = '';
+        }
+        
         
     } //end function (function expressions auto-bind this method to the class. Then you won't have to 'bind' down below.)
+
+    checkForDupe = ( event ) => {
+        const newUserName = document.getElementById('usernameIn').value;
+        document.getElementById( 'addUsernameButton').disabled = this.state.usernames.includes (newUserName); 
+
+    }
     render() {
         //use map to map this.state.usenames to li elements
         const users =  this.state.usernames.map(
@@ -29,8 +41,8 @@ class Body extends Component {
         return (
             <div>
                 <h1> Add Username </h1>
-                <input type="text" placeholder="username" id="usernameIn"></input> 
-                <button onClick={ this.addUsername }> Add Username </button>
+                <input type="text" placeholder="username" id="usernameIn" onChange={ this.checkForDupe }></input> 
+                <button onClick={ this.addUsername } id="addUsernameButton"> Add Username </button>
                 <ul> 
                 { users }
                 </ul>
